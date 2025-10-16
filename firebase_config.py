f44cd1b8cab8811af9c837f2606cb2f089745236
Bblasio <ochiengblasio@gmail.com>
2025-10-16 21:52:55 +0300

    .

--- firebase_config.py
+++ firebase_config.py
@@ -7,5 +7,6 @@ firebaseConfig = {
     "storageBucket": "goat-smart-farm.appspot.com",  # ✅ corrected as well
     "messagingSenderId": "308912834329",
     "appId": "1:308912834329:web:b7b38370133b4fd346a788",
-    "databaseURL": "https://goat-smart-farm-default-rtdb.firebaseio.com"  # ✅ added correct URL
+    "databaseURL": "ENV_VARIABLE"
+  # ✅ added correct URL
 }
